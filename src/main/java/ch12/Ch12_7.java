632eedefcfa4463320c192389519cb44a6c503af
mitsui3point <rladbsgh0331@gmail.com>
2023-01-17 14:19:15 +0900

    feat(@Ch12): add Annotation basic

---
+++ src/main/java/ch12/Ch12_7.java
@@ -0,0 +1,30 @@
+package ch12;
+
+public class Ch12_7 {
+    public static void main(String[] args) {
+        new Child().parentMethod();
+        new Child().parentmethod();
+    }
+}
+class Parent {
+    void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+
+}
+
+/**
+ *
+ * @Override
+ * : {@link Override}<br>
+ * : 오버라이딩을 올바르게 했는지 컴파일러가 체크하게 한다.<br>
+ * : 오버라이딩할 때 메서드이름을 잘못 적는 실수를 하는 경우가 많다.<br>
+ * : 오버라이딩 할 때는 메서드 선언부 앞에 @Override 를 붙이자.
+ */
+class Child extends Parent {
+    //@Override
+    public void parentmethod() {
+        //부모의 이름을 잘못 적었기 때문에 Override 되지 않음, @Override 애노테이션이 있을 경우 컴파일 오류
+        System.out.println("Child.parentMethod");
+    }
+}
