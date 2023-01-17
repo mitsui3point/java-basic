ffaf167370964024d4dc36960bbc9e398164774c
mitsui3point <rladbsgh0331@gmail.com>
2023-01-17 14:40:49 +0900

    feat($Ch12): update @Deprecated Annotation

--- src/main/java/ch12/Ex12_7.java
+++ src/main/java/ch12/Ex12_7.java
@@ -1,9 +1,10 @@
 package ch12;
 
+import java.util.Date;
+
 public class Ex12_7 {
     public static void main(String[] args) {
         new Child().parentMethod();
-        new Child().parentmethod();
     }
 }
 class Parent {
@@ -14,17 +15,31 @@ class Parent {
 }
 
 /**
- *
  * @Override
  * : {@link Override}<br>
  * : 오버라이딩을 올바르게 했는지 컴파일러가 체크하게 한다.<br>
  * : 오버라이딩할 때 메서드이름을 잘못 적는 실수를 하는 경우가 많다.<br>
  * : 오버라이딩 할 때는 메서드 선언부 앞에 @Override 를 붙이자.
+ *
+ * @Deprecated
+ * : {@link Deprecated}<br>
+ * : 앞으로 사용하지 않을 것을 권장하는 필드나 메서드에 붙인다.<br>
+ * : @Deprecated 사용 예, {@link Date#getDate()}<br>
+ * : @Deprecated 가 붙은 대상이 사용된 코드를 컴파일하면 나타나는 메시지<br>
+ * javac -encoding utf-8 src/main/java/ch12/Ex12_7.java<br>
+ * Note: src\main\java\ch12\Ex12_7.java uses or overrides a deprecated API.<br>
+ * Note: Recompile with -Xlint:deprecation for details.<br>
+ * <br>
+ * javac -encoding utf-8 -Xlint:deprecation src/main/java/ch12/Ex12_7.java<br>
+ * src\main\java\ch12\Ex12_7.java:7: warning: [deprecation] parentMethod() in Child has been deprecated<br>
+ *         new Child().parentMethod();<br>
+ *                    ^<br>
+ * 1 warning<br>
  */
 class Child extends Parent {
-    //@Override
-    public void parentmethod() {
-        //부모의 이름을 잘못 적었기 때문에 Override 되지 않음, @Override 애노테이션이 있을 경우 컴파일 오류
+    @Override
+    @Deprecated
+    public void parentMethod() {
         System.out.println("Child.parentMethod");
     }
 }
